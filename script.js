663afbb15bd8f568cd837caa7c145e3ad91444d2
consultingspain <nmjbrandmarketing@gmail.com>
2025-05-15 16:09:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('language').addEventListener('change', function() {
+    var selectedLanguage = this.value;
+    console.log("Idioma seleccionado: " + selectedLanguage);
+    // Aquí podrías cambiar el contenido del sitio según el idioma seleccionado
+});
